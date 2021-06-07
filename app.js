93e75b93decc4f01eb55fd44b59301f8e25b80ba
gerwo <mustafa82@yandex.ru>
2021-06-07 14:13:56 +0300

    added cors

--- app.js
+++ app.js
@@ -48,8 +48,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
-app.use(cors(corsConfig));
-app.options('*', cors(corsConfig));
+app.use('*', cors(corsConfig));
 
 app.use(routes);
 
